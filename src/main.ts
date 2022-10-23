9d432c2109c89a74f4187394205bc451d51e36f0
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2022-10-23 15:13:10 +0900

    Add background image

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import * as THREE from "three"
+import * as THREE from 'three';
 
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  50,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  2000
+);
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-console.log(scene);
+document.body.appendChild(renderer.domElement);
+renderer.render(scene, camera);
