a01acdfa7e08de8b59c6a3068320f454eb72c3d5
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2022-10-23 22:55:04 +0900

    Update file path

--- src/mesh/ball.ts
+++ src/mesh/ball.ts
@@ -4,10 +4,11 @@ export const createBall = () => {
   const ballGeometry = new THREE.SphereGeometry(100, 64, 32);
   // https://threejs.org/docs/index.html?q=geome#api/en/geometries/SphereGeometry
 
+  const rowContentEndpoint =
+    'https://raw.githubusercontent.com/Liyuu8/threejs-vite-project/main/';
+  const imagePath = 'src/images/textures/earth.jpg';
   const texture = new THREE.TextureLoader().load(
-    import.meta.env.DEV
-      ? 'src/images/textures/earth.jpg'
-      : 'https://raw.githubusercontent.com/Liyuu8/threejs-vite-project/main/src/images/textures/earth.jpg'
+    (import.meta.env.DEV ? '' : rowContentEndpoint) + imagePath
   );
 
   const ballMaterial = new THREE.MeshPhysicalMaterial({ map: texture });
