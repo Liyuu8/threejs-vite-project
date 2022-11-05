f05da4e8c9151544c38be9ec2f6d96008bb90a6c
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2022-11-05 14:46:44 +0900

    Updates

---
+++ assets/ambient_light.94d1322a.js
@@ -0,0 +1,1 @@
+import{A as t}from"./main.f3d22554.js";const n=()=>new t(16777215,.8);export{n as c};
---
+++ assets/geometry.721502d9.js
@@ -0,0 +1,1 @@
+import{P as r}from"./main.f3d22554.js";const t=()=>{const e=new r(75,window.innerWidth/window.innerHeight,.1,100);return e.position.set(1,1,2),e};export{t as c};
