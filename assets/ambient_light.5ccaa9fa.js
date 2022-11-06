32a5f119406cc7a2fef6dbde7a79870a59e56d91
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2022-11-06 16:19:20 +0900

    Updates

---
+++ assets/ambient_light.5ccaa9fa.js
@@ -0,0 +1,1 @@
+import{P as a,A as r}from"./main.86866a7f.js";const s=(e,t,i)=>{const n=new a(75,window.innerWidth/window.innerHeight,.1,100);return n.position.set(e,t,i),n},c=e=>new r(16777215,e);export{s as a,c};
